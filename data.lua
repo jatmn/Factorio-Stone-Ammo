cb92a225d28a8a9b4c5331b1c66c0d86952d2186
jatmn <the@jat.mn>
2026-02-13 20:10:46 -0800

    Updates:     - Moved ammo placement in crafting menu to before yellow ammo.     - Updated code style to Stylua   Fixes:     - Fixed split recipe and ammo item in factoriopedia

--- data.lua
+++ data.lua
@@ -11,63 +11,64 @@ local j_sa_damage = settings.startup["j_sa_damage"].value
 
 local item_sounds = require("__base__.prototypes.item_sounds")
 
-
 data:extend({
-  {
+	{
 		type = "recipe",
-		name = "j_cheap_stone_ammo_recipe",
+		name = "j_cheap_stone_ammo",
 		category = "crafting",
 		enabled = true,
-    hidden = false,
+		hidden = false,
 		energy_required = 0.5,
 		ingredients = {
-		  {type = "item", name = "stone", amount = j_sa_stone},
+			{ type = "item", name = "stone", amount = j_sa_stone },
 		},
-		results = 
-    {
-			{type = "item", name = "j_cheap_stone_ammo", amount = j_sa_clips},	
+		results = {
+			{ type = "item", name = "j_cheap_stone_ammo", amount = j_sa_clips },
 		},
-  },
+	},
 })
 
-
 local j_cheap_stone_ammo = {
-  type = "ammo",
-  name = "j_cheap_stone_ammo",
-  icon = "__jatmn_stone_ammo__/graphics/icons/Stone-firearm-magazine.png",
-  icon_size = 64,
-  icon_mipmaps = 4,
-  ammo_category = "bullet",
-  ammo_type = {
-    action = {{
-      type = "direct",
-      action_delivery = {{
-        type = "instant",
-        source_effects = {{ type = "create-explosion", entity_name = "explosion-gunshot" }},
-        target_effects = {
-          {
-            type = "create-entity",
-            entity_name = "explosion-hit",
-            offsets = {{ 0, 1 }},
-            offset_deviation = {{ -0.5, -0.5 }, { 0.5, 0.5 }}
-          },
-          {type = "damage", damage = {amount = j_sa_damage, type = "physical"}},
-        }
-      }}
-    }}
-  },
-  magazine_size = j_sa_size,
-  subgroup = "ammo",
-  order = "a[basic-clips]-a[firearm-magazine]",
-  inventory_move_sound = item_sounds.ammo_small_inventory_move,
-  pick_sound = item_sounds.ammo_small_inventory_pickup,
-  drop_sound = item_sounds.ammo_small_inventory_move,
-  stack_size = 200,
-  weight = 5*kg
+	type = "ammo",
+	name = "j_cheap_stone_ammo",
+	icon = "__jatmn_stone_ammo__/graphics/icons/Stone-firearm-magazine.png",
+	icon_size = 64,
+	icon_mipmaps = 4,
+	ammo_category = "bullet",
+	ammo_type = {
+		action = {
+			{
+				type = "direct",
+				action_delivery = {
+					{
+						type = "instant",
+						source_effects = { { type = "create-explosion", entity_name = "explosion-gunshot" } },
+						target_effects = {
+							{
+								type = "create-entity",
+								entity_name = "explosion-hit",
+								offsets = { { 0, 1 } },
+								offset_deviation = { { -0.5, -0.5 }, { 0.5, 0.5 } },
+							},
+							{ type = "damage", damage = { amount = j_sa_damage, type = "physical" } },
+						},
+					},
+				},
+			},
+		},
+	},
+	magazine_size = j_sa_size,
+	subgroup = "ammo",
+	order = "a1[basic-clips]-a[firearm-magazine]",
+	inventory_move_sound = item_sounds.ammo_small_inventory_move,
+	pick_sound = item_sounds.ammo_small_inventory_pickup,
+	drop_sound = item_sounds.ammo_small_inventory_move,
+	stack_size = 200,
+	weight = 5 * kg,
 }
 if j_sa_kb_behaviour == "Knockback" then
-  local target_effects = j_cheap_stone_ammo.ammo_type.action[1].action_delivery[1].target_effects
-  target_effects[#target_effects + 1] = { type = "push-back", distance = 3 }
+	local target_effects = j_cheap_stone_ammo.ammo_type.action[1].action_delivery[1].target_effects
+	target_effects[#target_effects + 1] = { type = "push-back", distance = 3 }
 end
 
 data:extend({ j_cheap_stone_ammo })
--- settings.lua
+++ settings.lua
@@ -1,44 +1,42 @@
 data:extend({
-    {
-        type = "string-setting",
-        name = "j_sa_kb_behaviour",
-        setting_type = "startup",
-        order = "a1",
+	{
+		type = "string-setting",
+		name = "j_sa_kb_behaviour",
+		setting_type = "startup",
+		order = "a1",
 		default_value = "Knockback",
-		allowed_values =  {"Knockback", "No Knockback"},
-    },
-    {
-        type = "int-setting",
-        name = "j_sa_damage",
-        setting_type = "startup",
-        order = "a2",
+		allowed_values = { "Knockback", "No Knockback" },
+	},
+	{
+		type = "int-setting",
+		name = "j_sa_damage",
+		setting_type = "startup",
+		order = "a2",
 		default_value = 1,
-        minimum_value = 1
-    },
-    {
-        type = "int-setting",
-        name = "j_sa_stone",
-        setting_type = "startup",
-        order = "a3",
+		minimum_value = 1,
+	},
+	{
+		type = "int-setting",
+		name = "j_sa_stone",
+		setting_type = "startup",
+		order = "a3",
 		default_value = 5,
-        minimum_value = 1
-    },
-    {
-        type = "int-setting",
-        name = "j_sa_size",
-        setting_type = "startup",
-        order = "a4",
+		minimum_value = 1,
+	},
+	{
+		type = "int-setting",
+		name = "j_sa_size",
+		setting_type = "startup",
+		order = "a4",
 		default_value = 10,
-        minimum_value = 1
-    },
-    {
-        type = "int-setting",
-        name = "j_sa_clips",
-        setting_type = "startup",
-        order = "a5",
+		minimum_value = 1,
+	},
+	{
+		type = "int-setting",
+		name = "j_sa_clips",
+		setting_type = "startup",
+		order = "a5",
 		default_value = 10,
-        minimum_value = 1
-    },
-
-
+		minimum_value = 1,
+	},
 })
